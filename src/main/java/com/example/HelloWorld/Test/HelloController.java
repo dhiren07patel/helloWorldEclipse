6200c16c924855b1f1a35b148e7da076f51693ce
Dhiren patel <dhiren@avdevs.com>
2023-09-25 13:31:40 +0530

    Hello Controller change

--- src/main/java/com/example/HelloWorld/Test/HelloController.java
+++ src/main/java/com/example/HelloWorld/Test/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		System.out.println("\"Greetings from Spring Boot Eclipse!\"");
+		System.out.println("\"Greetings from Spring Boot Eclipse! AND Deployed On Good cloud");
 		return "Greetings from Spring Boot Eclipse! + Deployed On Good cloud";
 	}
 
