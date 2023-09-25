2e7b0846e2f5fcde4fd5fd93669a6c01d30b2428
Dhiren patel <dhiren@avdevs.com>
2023-09-25 13:43:58 +0530

    Move file to source folder

--- src/main/java/com/example/HelloWorld/HelloController.java
+++ src/main/java/com/example/HelloWorld/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.HelloWorld.Test;
+package com.example.HelloWorld;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
