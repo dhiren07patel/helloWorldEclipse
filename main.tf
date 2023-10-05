24ff085b8e9f00154d58c0fb36db009baba2f0f0
Dhiren patel <dhiren@avdevs.com>
2023-10-05 11:26:41 +0530

    Change the yaml and tf file

--- main.tf
+++ main.tf
@@ -1,9 +1,21 @@
 provider "google" {
   credentials = file("klara-comm-nonprod-b8db0085dec5.json")
   project     = "klara-comm-nonprod"
-  #region      = "us-central1"  # Adjust to your desired region
+  region      = "us-central1"  # Adjust to your desired region
 }
 
+resource "google_cloud_run_service" "run_service" {
+  name = "helloWorldEclipse"
+  location = "us-europe-west1"
+
+  template {
+	spec {
+  	containers {
+    	image = "gcr.io/klara-comm-nonprod/helloworldeclipse@sha256:eb4bbb84867ccb6f9e0db68f175034a02f2d5ca1badd5af2ad360ca5fefbd19d"
+  	}
+	}
+  }
+}
 
 resource "google_pubsub_topic" "helloWorldEclipse-service" {
   name = "helloWorldEclipse-service"
